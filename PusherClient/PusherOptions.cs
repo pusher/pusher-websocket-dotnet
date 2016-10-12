e5974841355a6a8fe4ac396e379271cd8a93d524
Tomasz Jagusz <tomasz.jagusz@presco.pl>
2016-10-12 08:05:34 +0200

    Set default cluster to mt1

--- PusherClient/PusherOptions.cs
+++ PusherClient/PusherOptions.cs
@@ -4,7 +4,7 @@
     {
         public bool Encrypted = false;
         public IAuthorizer Authorizer = null;
-        public string Cluster = "eu";
+        public string Cluster = "mt1";
         internal string Host
         {
             get { return string.Format("ws-{0}.pusher.com", this.Cluster); }
