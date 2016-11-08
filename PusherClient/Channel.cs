0f6ccc5e84eb666cb9cc626c1cab370cfcf67b80
Callum Oakley <callum@pusher.com>
2016-11-08 15:48:18 +0000

    removed extraneous imports

--- PusherClient/Channel.cs
+++ PusherClient/Channel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PusherClient
+﻿namespace PusherClient
 {
     public delegate void SubscriptionEventHandler(object sender);
 
