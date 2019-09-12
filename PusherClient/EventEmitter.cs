f5b9793fe13dc420bdb78cb69657dbf18fe02393
Collin Dauphinee <cdauphinee@scopely.com>
2019-09-11 19:10:50 -0700

    Adding EventEmitter support for listening to raw messages

--- PusherClient/EventEmitter.cs
+++ PusherClient/EventEmitter.cs
@@ -12,6 +12,9 @@ namespace PusherClient
         private readonly Dictionary<string, List<Action<dynamic>>> _eventListeners = new Dictionary<string, List<Action<dynamic>>>();
         private readonly List<Action<string, dynamic>> _generalListeners = new List<Action<string, dynamic>>();
 
+        private readonly Dictionary<string, List<Action<string>>> _rawEventListeners = new Dictionary<string, List<Action<string>>>();
+        private readonly List<Action<string, string>> _rawGeneralListeners = new List<Action<string, string>>();
+
         /// <summary>
         /// Binds to a given event name
         /// </summary>
@@ -31,6 +34,24 @@ namespace PusherClient
         }
 
         /// <summary>
+        /// Binds to a given event name. The listener will receive the raw JSON message.
+        /// </summary>
+        /// <param name="eventName">The Event Name to listen for</param>
+        /// <param name="listener">The action to perform when the event occurs</param>
+        public void Bind(string eventName, Action<string> listener)
+        {
+            if (_rawEventListeners.ContainsKey(eventName))
+            {
+                _rawEventListeners[eventName].Add(listener);
+            }
+            else
+            {
+                var listeners = new List<Action<string>> { listener };
+                _rawEventListeners.Add(eventName, listeners);
+            }
+        }
+
+        /// <summary>
         /// Binds to ALL event
         /// </summary>
         /// <param name="listener">The action to perform when the any event occurs</param>
@@ -40,12 +61,22 @@ namespace PusherClient
         }
 
         /// <summary>
+        /// Binds to ALL event. The listener will receive the raw JSON message.
+        /// </summary>
+        /// <param name="listener">The action to perform when the any event occurs</param>
+        public void BindAll(Action<string, string> listener)
+        {
+            _rawGeneralListeners.Add(listener);
+        }
+
+        /// <summary>
         /// Removes the binding for the given event name
         /// </summary>
         /// <param name="eventName">The name of the event to unbind</param>
         public void Unbind(string eventName)
         {
             _eventListeners.Remove(eventName);
+            _rawEventListeners.Remove(eventName);
         }
 
         /// <summary>
@@ -55,9 +86,22 @@ namespace PusherClient
         /// <param name="listener">The action to remove</param>
         public void Unbind(string eventName, Action<dynamic> listener)
         {
-            if(_eventListeners.ContainsKey(eventName))
+            if (_eventListeners.ContainsKey(eventName))
             {
                 _eventListeners[eventName].Remove(listener);
+            }
+        }
+
+        /// <summary>
+        /// Remove the action for the event name
+        /// </summary>
+        /// <param name="eventName">The name of the event to unbind</param>
+        /// <param name="listener">The action to remove</param>
+        public void Unbind(string eventName, Action<string> listener)
+        {
+            if (_rawEventListeners.ContainsKey(eventName))
+            {
+                _rawEventListeners[eventName].Remove(listener);
             }
         }
 
@@ -68,23 +112,42 @@ namespace PusherClient
         {
             _eventListeners.Clear();
             _generalListeners.Clear();
+
+            _rawEventListeners.Clear();
+            _rawGeneralListeners.Clear();
         }
 
         internal void EmitEvent(string eventName, string data)
         {
-            var obj = JsonConvert.DeserializeObject<dynamic>(data);
-
-            // Emit to general listeners regardless of event type
-            foreach (var action in _generalListeners)
+            foreach (var action in _rawGeneralListeners)
             {
-                action(eventName, obj);
+                action(eventName, data);
             }
 
-            if (_eventListeners.ContainsKey(eventName))
+            if (_rawEventListeners.ContainsKey(eventName))
             {
-                foreach (var action in _eventListeners[eventName])
+                foreach (var action in _rawEventListeners[eventName])
                 {
-                    action(obj);
+                    action(data);
+                }
+            }
+
+            // Don't bother with deserialization if there are no dynamic listeners
+            if (_generalListeners.Count > 0 || _eventListeners.Count > 0)
+            {
+                var obj = JsonConvert.DeserializeObject<dynamic>(data);
+
+                foreach (var action in _generalListeners)
+                {
+                    action(eventName, obj);
+                }
+
+                if (_eventListeners.ContainsKey(eventName))
+                {
+                    foreach (var action in _eventListeners[eventName])
+                    {
+                        action(obj);
+                    }
                 }
             }
         }
