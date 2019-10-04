128bfde46fd6f4c517dae5f2f1ecdaf1d3fd74fa
John McLoughlin <JohnMc@BatfishSolutions.com>
2019-10-04 11:10:31 +0100

    Removed outputpath parameter from build package

--- package.cmd
+++ package.cmd
@@ -1,3 +1,3 @@
 "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\amd64\msbuild.exe" pusher-dotnet-client.sln /t:Clean,Rebuild /p:Configuration=Release /fileLogger
 
-"%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\amd64\msbuild.exe" PusherClient/PusherClient.csproj /t:pack /p:configuratio=release /p:OutputPath=.\
+"%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\amd64\msbuild.exe" PusherClient/PusherClient.csproj /t:pack /p:configuration=release
